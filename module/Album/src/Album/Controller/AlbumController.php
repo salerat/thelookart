098b297d49e19b29a704265e2b6276608a549de4
salerat <salerat@gmail.com>
2013-05-24 21:55:55 +0700

    Первая версия TheLookArt

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,37 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    protected $albumTable;
+    public function indexAction()
+    {
+        return new ViewModel(array(
+            'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+    public function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }[7:01:48 PM] Дмитрий: В гайдах меняешь цвет по схеме которую оговорили, что переделываем, чтобы можно было его стиль менять в одном классе?
+        return $this->albumTable;
+    }
+
+}
